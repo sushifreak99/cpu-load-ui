e7eb858a62a1fbc05617ad2308236ba1d4907e74
sushifreak99 <boaz.ost.dev.24@gmail.com>
2023-08-11 02:01:50 +0300

    Added charts and integrated with data
    Signed-off-by: sushifreak99 <boaz.ost.dev.24@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { Line } from 'react-chartjs-2';
 import './App.css'
 import { DEFAULT_BUFFER_SIZE, useCpuTimeSeries } from './utils/cpuLoadBuffer'
 import { usePolling } from './utils/polling'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 let x = 0;
 function App() {
@@ -10,8 +31,37 @@ function App() {
     x += 1;
   }, 1000)
 
+
   return (
-    <ul>{cpuLoad.map(({value, time}, ix) => <li>{ix}: value {value} time {time}</li>)}</ul>
+    <Line
+      options={{
+        spanGaps: true,
+        responsive: true,
+        scales: {
+          yAxis: {
+            max: 1.05,
+            min: 0,
+            position: 'right',
+            display: false,
+          }
+        },
+      }}
+      width={'1200'}
+      height={'400'}
+      data={{
+        labels: cpuLoad.map(({ time } ) => time),
+        datasets: [
+          {
+            label: 'CPU avg. load',
+            data: cpuLoad.map(({ value }) => value),
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            pointRadius: 5,
+            pointHoverRadius: 7,
+          }
+        ],
+      }}
+    />
   )
 }
 
